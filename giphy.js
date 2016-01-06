c364e712c177f7f61d595e02d43289c5dbbd846c
2xAA <hello@brkbrkbrk.com>
2016-01-06 13:40:41 +0000

    Refactors to avoid prototyping problems

--- giphy.js
+++ giphy.js
@@ -4,253 +4,239 @@
 		self.key = key;
 		self.version = 1;
 		self.url = 'https://api.giphy.com/v' + self.version + '/';
-	};
+		self.stickers = {};
 	
-	// Search
-	// --
-	// https://github.com/Giphy/GiphyAPI#search-endpoint
-	giphy.prototype.search = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'gifs/search';
-		
-		// Check for required parameters
-		if('q' in params) {
-			url += '?q=' + params.q;
-			delete params.q;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// Get GIF by ID
-	// --
-	// https://github.com/Giphy/GiphyAPI#get-gifs-by-id-endpoint
-	giphy.prototype.gif = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'gifs/';
-		
-		// Check for required parameters
-		if('id' in params) {
-			url += params.id;
-			delete params.id;
-		} else {
-			var eMsg = 'giphy.js: No ID.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// Get GIFs by ID
-	// --
-	// https://github.com/Giphy/GiphyAPI#get-gif-by-id-endpoint
-	giphy.prototype.gifs = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'gifs?ids=';
-		
-		// Check for required parameters
-		if('ids' in params) {
-			params.ids.forEach(function(id, idx) {
-				url += id;
-				if(idx+1 != params.ids.length) url += ',';
-			});
-			delete params.ids;
-		} else {
-			var eMsg = 'giphy.js: No IDs.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// Translate
-	// --
-	// https://github.com/Giphy/GiphyAPI#translate-endpoint
-	giphy.prototype.translate = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'gifs/translate';
-		
-		// Check for required parameters
-		if('s' in params) {
-			url += '?s=' + params.s.replace(' ', '+');
-			delete params.s;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// Random
-	// --
-	// https://github.com/Giphy/GiphyAPI#random-endpoint
-	giphy.prototype.random = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'gifs/random';
-		
-		// Check for required parameters
-		if('tag' in params) {
-			url += '?tag=' + params.tag;
-			delete params.tag;
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// Trending GIFs
-	// --
-	// https://github.com/Giphy/GiphyAPI#trending-gifs-endpoint
-	giphy.prototype.trending = function(succCb, errCb) {
-		var self = this;
-		var url = 'gifs/trending';
-		
-		self.request(url, {}, succCb, errCb);
-	};
-
-	// STICKER API
-	// --
-	// https://github.com/Giphy/GiphyAPI#giphy-sticker-api
-	var stickers = function() {
-		console.log(this);
-	};
-
-	stickers.prototype = giphy.prototype;
-
-	// STICKER Search
-	// --
-	// https://github.com/Giphy/GiphyAPI#sticker-search-endpoint
-	stickers.prototype.search = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'stickers/search';
-		
-		// Check for required parameters
-		if('q' in params) {
-			url += '?q=' + params.q;
-			delete params.q;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// STICKER Roulette (Random)
-	// --
-	// https://github.com/Giphy/GiphyAPI#sticker-roulette-random-endpoint
-	stickers.prototype.roulette = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'stickers/roulette';
-		
-		// Check for required parameters
-		if('tag' in params) {
-			url += '?tag=' + params.tag;
-			delete params.tag;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// STICKER Trending
-	// --
-	// https://github.com/Giphy/GiphyAPI#sticker-trending-endpoint
-	stickers.prototype.trending = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'stickers/trending';
-		
-		// Check for required parameters
-		if('s' in params) {
-			url += '?s=' + params.s;
-			delete params.s;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-
-	// STICKER Translate
-	// -- 
-	// https://github.com/Giphy/GiphyAPI#sticker-translate-endpoint
-	stickers.prototype.translate = function(params, succCb, errCb) {
-		var self = this;
-		var url = 'stickers/translate';
-		
-		// Check for required parameters
-		if('s' in params) {
-			url += '?s=' + params.s;
-			delete params.s;
-		} else {
-			var eMsg = 'giphy.js: No query.';
-			if(errCb) errCb(eMsg);
-			else console.error(eMsg);
-		}
-		
-		self.request(url, params, succCb, errCb);
-	};
-	
-	// Request
-	giphy.prototype.request = function(urlParams, params, succCb, errCb) {
-		var self = this;
-		var url = self.url;
-		var hasStartingValue = false;
-
-		url += urlParams;
-
-		// Check for starting '?'
-		if(url.indexOf('?') > -1) hasStartingValue = true;
-		
-		for(var key in params) {
-			if(hasStartingValue) {
-				url += '&' + key + '=' + params[key];
+		// Search
+		// --
+		// https://github.com/Giphy/GiphyAPI#search-endpoint
+		self.search = function(params, succCb, errCb) {
+			var url = 'gifs/search';
+			
+			// Check for required parameters
+			if('q' in params) {
+				url += '?q=' + params.q;
+				delete params.q;
 			} else {
-				url += '?' + key + '=' + params[key];
-				hasStartingValue = true;
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
 			}
-		}
-		
-		if(hasStartingValue) {
-			url += '&api_key=' + self.key;
-		} else {
-			url += '?api_key=' + self.key;
-		}
-		
-		console.log(url);
-
-		var req = new XMLHttpRequest();
-		req.open("GET", url, true);
-		req.responseType = "json";
-		req.onload = function () {
-			var status = req.status;
-			if (status == 200) {
-
-				if(succCb) succCb(req.response);
-
-			} else {
-				if(errCb) errCb(status);
-		 		else console.log(status);
-			}
+			
+			self.request(url, params, succCb, errCb);
 		};
-		req.send();
 
-		return url;
+		// Get GIF by ID
+		// --
+		// https://github.com/Giphy/GiphyAPI#get-gifs-by-id-endpoint
+		self.gif = function(params, succCb, errCb) {
+			var url = 'gifs/';
+			
+			// Check for required parameters
+			if('id' in params) {
+				url += params.id;
+				delete params.id;
+			} else {
+				var eMsg = 'giphy.js: No ID.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}		
+			self.request(url, params, succCb, errCb);
+		};
+
+		// Get GIFs by ID
+		// --
+		// https://github.com/Giphy/GiphyAPI#get-gif-by-id-endpoint
+		self.gifs = function(params, succCb, errCb) {
+			var url = 'gifs?ids=';
+			
+			// Check for required parameters
+			if('ids' in params) {
+				params.ids.forEach(function(id, idx) {
+					url += id;
+					if(idx+1 != params.ids.length) url += ',';
+				});
+				delete params.ids;
+			} else {
+				var eMsg = 'giphy.js: No IDs.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// Translate
+		// --
+		// https://github.com/Giphy/GiphyAPI#translate-endpoint
+		self.translate = function(params, succCb, errCb) {
+			var url = 'gifs/translate';
+			
+			// Check for required parameters
+			if('s' in params) {
+				url += '?s=' + params.s.replace(' ', '+');
+				delete params.s;
+			} else {
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// Random
+		// --
+		// https://github.com/Giphy/GiphyAPI#random-endpoint
+		self.random = function(params, succCb, errCb) {
+			var url = 'gifs/random';
+			
+			// Check for required parameters
+			if('tag' in params) {
+				url += '?tag=' + params.tag;
+				delete params.tag;
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// Trending GIFs
+		// --
+		// https://github.com/Giphy/GiphyAPI#trending-gifs-endpoint
+		self.trending = function(succCb, errCb) {
+			var url = 'gifs/trending';
+			
+			self.request(url, {}, succCb, errCb);
+		};
+
+		// STICKER API
+		// --
+		// https://github.com/Giphy/GiphyAPI#giphy-sticker-api
+
+		// STICKER Search
+		// --
+		// https://github.com/Giphy/GiphyAPI#sticker-search-endpoint
+		self.stickers.search = function(params, succCb, errCb) {
+			var url = 'stickers/search';
+			
+			// Check for required parameters
+			if('q' in params) {
+				url += '?q=' + params.q;
+				delete params.q;
+			} else {
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// STICKER Roulette (Random)
+		// --
+		// https://github.com/Giphy/GiphyAPI#sticker-roulette-random-endpoint
+		self.stickers.roulette = function(params, succCb, errCb) {
+			var url = 'stickers/roulette';
+			
+			// Check for required parameters
+			if('tag' in params) {
+				url += '?tag=' + params.tag;
+				delete params.tag;
+			} else {
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// STICKER Trending
+		// --
+		// https://github.com/Giphy/GiphyAPI#sticker-trending-endpoint
+		self.stickers.trending = function(params, succCb, errCb) {
+			var url = 'stickers/trending';
+			
+			// Check for required parameters
+			if('s' in params) {
+				url += '?s=' + params.s;
+				delete params.s;
+			} else {
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+
+		// STICKER Translate
+		// -- 
+		// https://github.com/Giphy/GiphyAPI#sticker-translate-endpoint
+		self.stickers.translate = function(params, succCb, errCb) {
+			var url = 'stickers/translate';
+			
+			// Check for required parameters
+			if('s' in params) {
+				url += '?s=' + params.s;
+				delete params.s;
+			} else {
+				var eMsg = 'giphy.js: No query.';
+				if(errCb) errCb(eMsg);
+				else console.error(eMsg);
+			}
+			
+			self.request(url, params, succCb, errCb);
+		};
+		
+		// Request
+		self.request = function(urlParams, params, succCb, errCb) {
+			var self = this;
+			var url = self.url;
+			var hasStartingValue = false;
+
+			url += urlParams;
+
+			// Check for starting '?'
+			if(url.indexOf('?') > -1) hasStartingValue = true;
+			
+			for(var key in params) {
+				if(hasStartingValue) {
+					url += '&' + key + '=' + params[key];
+				} else {
+					url += '?' + key + '=' + params[key];
+					hasStartingValue = true;
+				}
+			}
+			
+			if(hasStartingValue) {
+				url += '&api_key=' + self.key;
+			} else {
+				url += '?api_key=' + self.key;
+			}
+			
+			console.log(url);
+
+			var req = new XMLHttpRequest();
+			req.open("GET", url, true);
+			req.responseType = "json";
+			req.onload = function () {
+				var status = req.status;
+				if (status == 200) {
+
+					if(succCb) succCb(req.response);
+
+				} else {
+					if(errCb) errCb(status);
+			 		else console.log(status);
+				}
+			};
+			req.send();
+
+			return url;
+		};
+
 	};
 
-	giphy.prototype.stickers = new stickers();
 	
 	$$['Giphy'] = giphy; // jshint ignore:line
 })(document, window);
